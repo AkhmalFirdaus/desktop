7d1ceb0a6c7070d359a2ef8eaeca83269513d555
Camila San <hello@camila.codes>
2018-10-10 16:40:36 +0200

    Adds OpenSSL initialization that it was commented out.
    Signed-off-by: Camila San <hello@camila.codes>

--- src/gui/owncloudgui.h
+++ src/gui/owncloudgui.h
@@ -129,10 +129,6 @@ private:
 
 
     QList<QAction *> _recentItemsActions;
-    
-    QSignalMapper *_folderOpenActionMapper;
-    QSignalMapper *_recentItemsMapper;
-    
     Application *_app;
 };
 
