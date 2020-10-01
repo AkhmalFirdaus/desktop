234ed01e3601c9bbd7a919275963dfa481c9bc4b
Kevin Ottens <kevin.ottens@nextcloud.com>
2020-10-01 17:51:17 +0200

    Remove dead code
    Signed-off-by: Kevin Ottens <kevin.ottens@nextcloud.com>

--- src/common/syncjournaldb.cpp
+++ src/common/syncjournaldb.cpp
@@ -2321,10 +2321,4 @@ QList<QString> SyncJournalDb::getSyncModePaths()
     return list;
 }
 
-void SyncJournalDb::emitSyncStatusChanged(QString path, bool status)
-{
-    qWarning() << Q_FUNC_INFO << "Syncing status changed for: " << path;
-    emit syncStatusChanged(path, status);
-}
-
 } // namespace OCC
--- src/common/syncjournaldb.h
+++ src/common/syncjournaldb.h
@@ -322,8 +322,6 @@ public:
 	*/
     qint64 secondsSinceLastAccess(const QString &path);
 
-    void emitSyncStatusChanged(QString path, bool status);
-
 private:
     int getFileRecordCount();
     bool updateDatabaseStructure();
@@ -410,9 +408,6 @@ private:
      * variable, for specific filesystems, or when WAL fails in a particular way.
      */
     QByteArray _journalMode;
-
-signals:
-    void syncStatusChanged(QString path, bool status);
 };
 
 bool OCSYNC_EXPORT
