stjordanis/client-1

Add socket command to upload a selection of files based on a regex

src/gui/socketapi/socketapi.h
@@ -121,6 +121,10 @@ private:
     Q_INVOKABLE void command_DELETE_ITEM(const QString &localFile, SocketListener *listener);
     Q_INVOKABLE void command_MOVE_ITEM(const QString &localFile, SocketListener *listener);
 
+    // External sync
+    Q_INVOKABLE void command_LIST_ACCOUNTS(const QString &argument, SocketListener *listener) const;
+    Q_INVOKABLE void command_UPLOAD_FILE(const QString &argument, SocketListener *listener) const;
+
     // Fetch the private link and call targetFun
     void fetchPrivateLinkUrlHelper(const QString &localFile, const std::function<void(const QString &url)> &targetFun);
 

AkhmalFirdaus/desktop

(cherry picked from commit 0ded3a56a9f3470a951b18eaa9d3c1b5e8db1135)

src/gui/socketapi/socketapi.h
@@ -129,6 +129,10 @@ private:
     Q_INVOKABLE void command_OPEN(const QString &localFile, SocketListener *listener);
 #endif
 
+    // External sync
+    Q_INVOKABLE void command_V2_LIST_ACCOUNTS(const QString &argument, SocketListener *listener) const;
+    Q_INVOKABLE void command_V2_UPLOAD_FILES_FROM(const QString &argument, SocketListener *listener) const;
+
     // Fetch the private link and call targetFun
     void fetchPrivateLinkUrlHelper(const QString &localFile, const std::function<void(const QString &url)> &targetFun);
 